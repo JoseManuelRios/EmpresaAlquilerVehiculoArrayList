8a20792aed0d1db8788ecfb185f8eda789263905
jriosaguilar <josemanueljbk99@gmail.com>
2019-03-01 12:17:34 +0100

    Modificación de la clase cliente para crear un cliente aleatorio con ArrayList

--- src/alquiler/Cliente.java
+++ src/alquiler/Cliente.java
@@ -5,6 +5,7 @@
  */
 package alquiler;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Random;
 
@@ -19,8 +20,6 @@ public class Cliente {
     private String nombre;
     private String apellidos;
     
-    
-    
     //Metodos
     public Cliente(String nif, String nombre, String apellidos) {
         this.nif = nif;
@@ -33,35 +32,63 @@ public class Cliente {
     public Cliente(){
         Random aleatorio=new Random();
         
-        char[] nif=new char[9];
-        String[] nombre={"Raul", "Jose", "Juan", "Hector", "Daniel", "Alejandro", "Victor", "Ana", "Maria", "Paula", "Roberta", "Lucia", "Lidia"};
-        String[] apellido={"Ramirez", "Rios", "Gonzalez", "Aguilar", "Garcia", "Lopez", "Ruiz", "Cabas", "Torres", "Bernal", "Sanchez", "Morales", "Sierra", "Perez"};
+        ArrayList<Character> nif=new ArrayList<>(9);
+        ArrayList<String> nombre=new ArrayList<>();
+        nombre.add("Raul");
+        nombre.add("Jose");
+        nombre.add("Juan");
+        nombre.add("Hector");
+        nombre.add("Daniel");
+        nombre.add("Alejandro");
+        nombre.add("Victor");
+        nombre.add("Ana");
+        nombre.add("María");
+        nombre.add("Paula");
+        nombre.add("Roberta");
+        nombre.add("Lucia");
+        nombre.add("Lidia");
+        
+        ArrayList<String> apellido=new ArrayList<>();
+        apellido.add("Ramírez");
+        apellido.add("Ríos");
+        apellido.add("González");
+        apellido.add("Aguilar");
+        apellido.add("García");
+        apellido.add("Lopez");
+        apellido.add("Ruiz");
+        apellido.add("Cabas");
+        apellido.add("Torres");
+        apellido.add("Bernal");
+        apellido.add("Sánchez");
+        apellido.add("Morales");
+        apellido.add("Sierra");
+        apellido.add("Perez");
         int numAlea;
         
         //Entramos en un bucle for para seleccionar los numeros del nif. Al salir del bucle 
         //le damos una letra aleatoria
-        for(int x=0;x<nif.length-1;x++){
+        for(int x=0;x<nif.size()-1;x++){
             numAlea=aleatorio.nextInt(10)+48;
-            nif[x]=(char) numAlea;
+            nif.add((char)numAlea);
         }
         
         //Al ultimo hueco del array nif le asignamos una letra aleatoria
         int letraAlea;
         letraAlea=aleatorio.nextInt(25)+65;
-        nif[8]=(char)letraAlea;
+        nif.add((char)letraAlea);
         
         this.nif="";
-        for(int x=0;x<nif.length;x++){
-            this.nif=this.nif+nif[x];
+        for(int x=0;x<nif.size();x++){
+            this.nif=this.nif+nif.get(x);
         }
         
         //Elegimos un numero aleatorio para seleccionar el nombre y dos aleatorios para los dos apellidos
-        int nom=aleatorio.nextInt(nombre.length-1);
-        int ape1=aleatorio.nextInt(apellido.length-1);
-        int ape2=aleatorio.nextInt(apellido.length-1);
+        int nom=aleatorio.nextInt(nombre.size()-1);
+        int ape1=aleatorio.nextInt(apellido.size()-1);
+        int ape2=aleatorio.nextInt(apellido.size()-1);
         
-        this.nombre=nombre[nom];
-        this.apellidos=apellido[ape1]+" "+apellido[ape2];
+        this.nombre=nombre.get(nom);
+        this.apellidos=apellido.get(ape1)+" "+apellido.get(ape2);
         
     }
     
